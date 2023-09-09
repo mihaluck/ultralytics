c8c054d31d5f541a84f7e7cef2e18f821c7f9fbc
Mikhail Egin <52533256+mihaluck@users.noreply.github.com>
2023-09-10 01:56:51 +0600

    Update val.py
    Исправил plot_predictions()

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -243,9 +243,8 @@ class DetectionValidator(BaseValidator):
 
     def plot_predictions(self, batch, preds, ni):
         """Plots predicted bounding boxes on input images and saves the result."""
-        plot_images(batch['img'],
+        plot_images(batch,
                     *output_to_target(preds, max_det=self.args.max_det),
-                    paths=batch['im_file'],
                     fname=self.save_dir / f'val_batch{ni}_pred.jpg',
                     names=self.names,
                     on_plot=self.on_plot)  # pred
