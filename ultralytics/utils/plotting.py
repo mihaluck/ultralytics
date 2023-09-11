2691f4505ee0582cdda3eca1c943f4cbbb02d325
Mikhail Egin <52533256+mihaluck@users.noreply.github.com>
2023-09-11 15:12:30 +0600

    Update plotting.py

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -441,11 +441,11 @@ def plot_images(images,
                 for j, box in enumerate(boxes.T.tolist()):
                     c = classes[j]
                     color = colors(c)
-                    iou = " " + str(iou[j])
+                    iou_j = " " + str(iou[j])            
                     c = names.get(c, c) if names else c
                     if labels or conf[j] > 0.25:  # 0.25 conf thresh
                         label = f'{c}' if labels else f'{c} {conf[j]:.1f}'
-                        annotator.box_label(box, label+iou, color=color)
+                        annotator.box_label(box, label+iou_j, color=color)
             elif len(classes):
                 for c in classes:
                     color = colors(c)
